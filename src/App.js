2800074e408e1a58440dd37019dd29533d45923f
ankitbatra2 <ankitbatra2202@gmail.com>
2021-01-05 02:57:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function GitHubUser({login}) {
+  const [data, setData] = useState(null); // use state hook
+  const[followers, test] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json()) // turn result into json
+    .then(setData) 
+    .then(function(follower){
+      return fetch('https://api.github.com/users/' + login + '/' + 'followers')
+    })
+    .then(follower => follower.json())
+    .then(test)
+    .catch(console.error);
+  }, []);
+
+  if(data) {
+    //return <div>{JSON.stringify(followers[0].login)}</div>
+
+    return (
+            <div>
+            <h1> GitHub Stats </h1>
+            <h1>GitHub Username: {data.login}</h1>
+            <img src={data.avatar_url} alt="image not found lol" width={300}></img>
+            <h2>Your most recent follower was: {followers[0].login}</h2>
+            <img src={`http://ghchart.rshah.org/${login}`}></img>
+            <Particles 
+              params={{ 
+                particles: { 
+                    number: { 
+                      value: 100, 
+                        density: { 
+                          enable: true, 
+                          value_area: 1000,
+                          }  
+                          }, 
+                      }, 
+                  }}/> 
+            </div>)
+          
+    
+    /*return <div>
+            <h1>{data.login}</h1>
+            <img src={data.avatar_url} alt=" not found lol"></img>
+            <h2>{data.bio}</h2>
+            <h3>
+            <img src="http://ghchart.rshah.org/ankitbatra22" alt="2016rshah's Github chart" />
+
+            <img src={`http://ghchart.rshah.org/ankitbatra22/${login}`}></img>
+          </div> */
+                }
+
+  return null;
 }
 
+
+
+function App(props) {
+  return (
+    <GitHubUser login='ankitbatra22' />
+  );
+  }
 export default App;
